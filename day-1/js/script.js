b116114e27556507d0a86016ad923cb9dab46b54
Jacob DiFede <novix1234@gmail.com>
2021-06-28 18:58:09 -0700

    day-1

---
+++ day-1/js/script.js
@@ -0,0 +1,18 @@
+// import { user } from './login.js'
+
+// const myData = "data";
+// const intitalString = "This is";
+// const closingPuncutation = "!";
+
+// console.log(`${intitalString}${user.name}'s${closingPuncutation}`);
+
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+}
+
+const jacobsCar = new Car("Nissan s15", 2001);
+
+console.log(jacobsCar)
