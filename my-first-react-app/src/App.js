d32f6e73fcdcd28ab2a902787ab2e839c464da02
Jacob DiFede <novix1234@gmail.com>
2021-07-14 21:10:45 -0700

    day-5

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import { MovieList } from './components/MovieList';
-import { Parent } from './components/Parent'
+import { MovieList } from './components/MovieList';
+// import { Parent } from './components/Parent'
 function App() {
 
   return (
     <div className="App">
-      {/* <MovieList/> */}
-      <Parent/>
+      <MovieList/>
+      {/* <Parent/> */}
     </div>
   );
 }
