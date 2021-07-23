c2cb07253f9d0a8a8ed30b0cab23a81d87595620
Jacob DiFede <novix1234@gmail.com>
2021-07-22 22:31:46 -0700

    Cleanup

--- Notes/day-1.js
+++ Notes/day-1.js
@@ -54,21 +54,25 @@
 
 // fetchMovieData();
 
-let myMovieList;
-let myMovie;
+// let myMovieList;
+// let myMovie;
 
-async function fetchMovieDataByName(title) {
-    const response = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=b1114fdf`);
-    myMovieList = response.json();
-}
+// async function fetchMovieDataByName(title) {
+//     const response = await fetch(`http://www.omdbapi.com/?s=${title}&apikey=b1114fdf`);
+//     myMovieList = response.json();
+// }
 
-async function fetchMovieDataById(id) {
-    const response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=b1114fdf`);
-    myMovie = response.json();
-}
+// async function fetchMovieDataById(id) {
+//     const response = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=b1114fdf`);
+//     myMovie = response.json();
+// }
 
-await fetchMovieDataByName("star wars");
-await fetchMovieDataById("tt0080684");
+// await fetchMovieDataByName("star wars");
+// await fetchMovieDataById("tt0080684");
 
-console.log(await myMovieList);
-console.log(await myMovie);
+// console.log(await myMovieList);
+// console.log(await myMovie);
+
+// export const user = {
+//     name: "jacob",
+// }
